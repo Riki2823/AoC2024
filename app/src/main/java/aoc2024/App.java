1dca054c9b13cd8255857c68b8b00e7989bc7fa2
Ricardo Villanueva <ricardo.villanueva@axisdata.net>
2024-12-03 17:22:28 +0100

    More changes

--- app/src/main/java/aoc2024/App.java
+++ app/src/main/java/aoc2024/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void main(String[] args) throws IOException {
         Day1 day1 = new Day1();
-        //day1.day1();
+        day1.day1();
 
         Day2 day2 = new Day2();
         day2.day2();
--- app/src/main/java/aoc2024/Day2.java
+++ app/src/main/java/aoc2024/Day2.java
@@ -7,7 +7,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day2 {
@@ -17,54 +19,86 @@ public class Day2 {
         String[] inputParsed = parseInput(FIRST_PART_PATH.getPath());
         List<String[]> input = new ArrayList<>();
 
-        for (String s : inputParsed){
+        for (String s : inputParsed) {
             input.add(StringUtils.split(s, " "));
         }
         int count = 0;
         for (String[] inputRow : input) {
             boolean isSave = true;
             boolean increasing = false;
-            if (Integer.parseInt(inputRow[0]) < Integer.parseInt(inputRow[1])){
+            if (Integer.parseInt(inputRow[0]) < Integer.parseInt(inputRow[1])) {
                 increasing = true;
             } else if (Integer.parseInt(inputRow[0]) == Integer.parseInt(inputRow[1])) {
-                continue;
-            }
-            for (int i = 0; i < inputRow.length; i++ ){
-                if (i == inputRow.length - 1) {
+                if (Integer.parseInt(inputRow[0]) + 1 == Integer.parseInt(inputRow[2])) {
+                    increasing = true;
+                } else if (Integer.parseInt(inputRow[0]) - 1 != Integer.parseInt(inputRow[2])) {
                     continue;
                 }
-                Integer first = Integer.parseInt(inputRow[i]);
-                Integer second = Integer.parseInt(inputRow[i+1]);
-
-                if (increasing){
-                    int difference = (first - second) * -1;
-                    if (difference > 3 || difference <= 0){
-                        isSave = false;
-                        break;
-                    }
-                    if (first > second){
-                        isSave = false;
-                        break;
-                    }
-                }else{
-
-                    int difference = (first - second);
-                    if (difference > 3 || difference <= 0){
-                        isSave = false;
-                        break;
-                    }
-                    if (first < second){
-                        isSave = false;
-                        break;
+            }
+            isSave = isSaveRow(inputRow, increasing, isSave);
+            if (isSave) {
+                count++;
+            } else {
+                for (int i = 0; i < inputRow.length; i++) {
+                    int value = Integer.parseInt(inputRow[i]);
+                    inputRow[i] = null;
+                    isSave = isSaveRow(inputRow, increasing, true);
+                    if (!isSave) {
+                        inputRow[i] = String.valueOf(value);
                     }
                 }
-            }
-            if (isSave){
-                count++;
             }
         }
         System.out.println("Save lines -> " + count);
+        System.out.println("Save lines fixed -> " + count);
 
+    }
+
+    private static boolean isSaveRow(String[] inputRow, boolean increasing, boolean isSave) {
+
+        for (int i = 0; i < inputRow.length; i++) {
+            Integer second = null;
+            if (i == inputRow.length - 1 | inputRow[i] == null) {
+                continue;
+            }
+
+            if (inputRow[i + 1] == null) {
+                if (i != inputRow.length - 2) {
+                    second = Integer.parseInt(inputRow[i + 2]);
+                } else if (i == inputRow.length - 2) {
+                    return false;
+                }
+            } else {
+                second = Integer.parseInt(inputRow[i + 1]);
+            }
+            Integer first = Integer.parseInt(inputRow[i]);
+
+
+            if (increasing) {
+                int difference = (first - second) * -1;
+                if (difference > 3 || difference <= 0) {
+                    isSave = false;
+                    break;
+                }
+
+                if (first > second) {
+                    isSave = false;
+                    break;
+                }
+
+            } else {
+                int difference = (first - second);
+                if (difference > 3 || difference <= 0) {
+                    isSave = false;
+                    break;
+                }
+                if (first < second) {
+                    isSave = false;
+                    break;
+                }
+            }
+        }
+        return isSave;
     }
 
     private String[] parseInput(String part1ProblemInput) throws IOException {
